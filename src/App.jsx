1c6021745d65478cfb7810ef94385e0914604918
somto <somto565@gmail.com>
2023-09-14 08:41:05 +0100

    changed env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { useEffect, useState } from "react";
 import Hero from "./components/Hero";
-import Features from "./components/Features";
+import Featured from "./components/Features";
 import MovieDetails from './components/movieDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const movieUrl = import.meta.env.VITE_MOVIEBOX_URL;
 
-const App = () => {
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [firstFiveMovies, setFirstFiveMovies] = useState([])
+
+  useEffect(() => {
+    fetch(movieUrl)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results.slice(0, 10))
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch(movieUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setFirstFiveMovies(data.results.slice(0, 5));
+      })
+  }, [])
+
+
+
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Hero firstFiveMovies={firstFiveMovies} movies={movies} setFirstFiveMovies={setFirstFiveMovies} setMovies={setMovies} />} />
-      <Route path="/featured" element={<Features movies={movies} setMovies={setMovies} />} />
+      <Route path="/featured" element={<Featured movies={movies} setMovies={setMovies} />} />
       <Route path="/movies/:id" element={<MovieDetails />} />
     </Routes>
     
   </Router>
+
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'; 
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import imdb from "../assets/imdb.png"
+import Navbar from './Navbar';
+import Featured from './Features';
+import Footer from '../components/Footer';
 
-const Hero = () => {
+
+const imgUrl = import.meta.env.VITE_MOVIEBOX_IMG;
+const CAROUSEL_DELAY = 3000;
+
+
+const Hero = ({ firstFiveMovies, setMovies, movies }) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % firstFiveMovies.length);
+      }, CAROUSEL_DELAY)
+    
+      return () => {
+        clearInterval(intervalId)
+      }
+    }, [firstFiveMovies])
+    
+
+
+
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <Navbar setMovies={setMovies}/>
+      <div className="relative w-full h-96">
+        <Carousel
+          autoPlay={true}
+          infiniteLoop={true}
+          interval={CAROUSEL_DELAY}
+          emulateTouch={true}
+          swipeable={false}
+          showThumbs={false}
+          showStatus={false}
+          showIndicators={false}
+          showArrows={true}
+          selectedItem={currentSlide}
+          onChange={(index) => setCurrentSlide(index)}
+        >
+          {firstFiveMovies.map((movieReq) => (
+            <div key={movieReq.id} className="w-full h-96">
+              <img src={imgUrl + movieReq.poster_path} alt={movieReq.title} className='bg-contain object-center'/>
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+              <div className="absolute inset-0 flex items-center justify-left px-20">
+                <div className="text-white text-left w-[400px] pt-16">
+                  <h1 className="text-2xl font-semibold py-2">{movieReq.title}</h1>
+                  <div className='flex flex-row'>
+                    <div className='flex flex-row py-2'>
+                      <img src={imdb} alt='img'/>
+                      <h6 className='px-6'>{movieReq.vote_average}</h6>
+                    </div>
+                  </div>
+                  <p className="text-sm py-2">{movieReq.overview}</p>
+                  <button className='bg-red-700 p-1 rounded-md px-3 text-white'>
+                    Watch Trailer
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <Featured movies={movies} setMovies={setMovies} />
+
+      <Footer />
+    </div>
+
+  );
+};
+
+Hero.propTypes = {
+    firstFiveMovies: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        poster_path: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        overview: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+
+    movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        poster_path: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        overview: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  
+    setMovies: PropTypes.func.isRequired,
+  };
 
 export default Hero
