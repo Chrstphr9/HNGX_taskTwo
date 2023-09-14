51ae95fdfb05822e1a35cfd32b5158fc2f22f0e6
somto <somto565@gmail.com>
2023-09-14 03:18:23 +0100

    routed pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import MovieDetails from './components/movieDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!!!!
-  </h1></div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Hero firstFiveMovies={firstFiveMovies} movies={movies} setFirstFiveMovies={setFirstFiveMovies} setMovies={setMovies} />} />
+      <Route path="/featured" element={<Features movies={movies} setMovies={setMovies} />} />
+      <Route path="/movies/:id" element={<MovieDetails />} />
+    </Routes>
+    
+  </Router>
   )
 }
 
